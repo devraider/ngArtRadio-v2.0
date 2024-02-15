c2ac6403614c28f6b2f779cc347adb2102776957
CrisTex <github@paulcristea.xyz>
2024-02-15 22:29:07 +0200

    Refactored welcome page

--- src/app/models/radio.ts
+++ src/app/models/radio.ts
@@ -4,7 +4,7 @@ export interface Radio {
     name: string,
     slug: string,
     type: string[],
-    descripton?: string,
+    description?: string,
     contact?: string,
     img: string,
     radioTypes: RadioType[]
--- src/app/welcome/services/welcome.service.ts
+++ src/app/welcome/services/welcome.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, map } from 'rxjs';
 import { Radio } from 'src/app/models/radio';
 
 @Injectable({
@@ -13,12 +13,19 @@ export class WelcomeService {
   
   
   #ROOT_API = environment.backendApi
-  #RADIO_API = `${this.#ROOT_API}/radio.json`
+  #RADIO_API = `${this.#ROOT_API}/radios`
   constructor(private _http: HttpClient) { }
 
   getRadioChannels(): void {
     this._http
-    .get<Radio[]>(this.#RADIO_API)
-    .subscribe((data) => this.#radioChannels$.next(data));
+    .get<ResponseRadio>(this.#RADIO_API)
+    .subscribe((data) => this.#radioChannels$.next(data._embedded.radios));
   }
+}
+
+
+interface ResponseRadio {
+    _embedded: {
+      radios: Radio[]
+    }
 }
